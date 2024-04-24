5ffabbd45212d51f56ff3691297a6147061fab7a
Alek30k <alemank23k@gmail.com>
2024-04-24 16:05:23 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,16 @@
+import { Search } from "lucide-react";
+import Image from "next/image";
+
 const Header = () => {
-  return <div className="">Header</div>;
+  return (
+    <div className="">
+      <Image src="/logo.png" alt="logo" width={200} height={200} />
+      <div className="">
+        <input type="text" className="" />
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
