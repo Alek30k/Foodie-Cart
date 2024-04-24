3db1aab551fa7d19e9d07139f9290bbb51044260
Alek30k <alemank23k@gmail.com>
2024-04-24 16:17:45 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
 import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="">
+    <div className="flex justify-between items-center p-6 md:px-20">
       <Image src="/logo.png" alt="logo" width={200} height={200} />
       <div className="flex border p-2 rounded-lg bg-gray-200 w-96">
         <input type="text" className="bg-transparent w-full" />
         <Search />
+      </div>
+      <div className="flex gap-5">
+        <Button variant="outline">Login</Button>
+        <Button>Sign Up</Button>
       </div>
     </div>
   );
