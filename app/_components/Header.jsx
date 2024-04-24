04912fc19787f3e3b65d75e2b458031b62519bd5
Alek30k <alemank23k@gmail.com>
2024-04-24 17:12:29 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,24 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { Search } from "lucide-react";
 import Image from "next/image";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="flex justify-between items-center p-6 md:px-20 shadow-sm ">
       <Image src="/logo.png" alt="logo" width={200} height={200} />
-      <div className="flex border p-2 rounded-lg bg-gray-200 w-96">
+      <div className="hidden md:flex border p-2 rounded-lg bg-gray-200 w-96">
         <input type="text" className="bg-transparent w-full" />
         <Search />
       </div>
-      <div className="flex gap-5">
-        <SignInButton mode="modal">
-          <Button variant="outline">Login</Button>
-        </SignInButton>
-        <SignUpButton>
-          <Button>Sign Up</Button>
-        </SignUpButton>
-      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-5">
+          <SignInButton mode="modal">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button>Sign Up</Button>
+          </SignUpButton>
+        </div>
+      )}
     </div>
   );
 };
