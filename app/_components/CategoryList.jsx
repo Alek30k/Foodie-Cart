a0d4bb9c9e60b7e23989191d2e840a5ae61a5630
Alek30k <alemank23k@gmail.com>
2024-04-25 17:07:30 -0300

    last commit

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
 import Image from "next/image";
+import { ArrowLeftCircle, ArrowRightCircle } from "lucide-react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const CategoryList = () => {
+  const listRef = useRef(null);
+
   const [categoryList, setCategoryList] = useState([]);
+  const params = useSearchParams();
+
+  useEffect(() => {
+    console.log(params.get("category"));
+  }, [params]);
+
   useEffect(() => {
     getCategoryList();
   }, []);
@@ -16,12 +27,30 @@ const CategoryList = () => {
     });
   };
 
+  const ScrollRightHandler = () => {
+    if (listRef.current) {
+      listRef.current.scrollBy({
+        left: 200,
+        behavior: "smooth",
+      });
+    }
+  };
+  // const ScrollLeftHandler = () => {
+  //   if (listRef.current) {
+  //     listRef.current.scrollBy({
+  //       right: 200,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  // };
+
   return (
-    <div className="mt-10">
-      <div className="flex gap-4 overflow-auto">
+    <div className="mt-10 relative">
+      <div className="flex gap-4 overflow-auto scrollbar-hide" ref={listRef}>
         {categoryList &&
           categoryList.map((category, index) => (
-            <div
+            <Link
+              href={"?category=" + category.slug}
               className="flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-orange-50 cursor-pointer group"
               key={index}
             >
@@ -30,12 +59,22 @@ const CategoryList = () => {
                 alt={category.name}
                 width={40}
                 height={40}
-                className="group-hover:scale-110 transition-all duration-2"
+                className="group-hover:scale-125 transition-all duration-2"
               />
-              <h2 className="text-sm font-medium">{category.name}</h2>
-            </div>
+              <h2 className="text-sm font-medium group-hover:text-primary">
+                {category.name}
+              </h2>
+            </Link>
           ))}
       </div>
+      <ArrowLeftCircle
+        className="absolute -left-10 top-9 bg-gray-500 rounded-full text-white w-8 h-8 cursor-pointer"
+        // onClick={() => ScrollLeftHandler()}
+      />
+      <ArrowRightCircle
+        className="absolute -right-10 top-9 bg-gray-500 rounded-full text-white w-8 h-8 cursor-pointer"
+        onClick={() => ScrollRightHandler()}
+      />
     </div>
   );
 };
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
   const { user, isSignedIn } = useUser();
 
   return (
-    <div className="flex justify-between items-center p-6 md:px-20 shadow-sm ">
+    <div className="flex justify-between items-center p-6 md:px-20 shadow-md">
       <Image src="/logo.png" alt="logo" width={200} height={200} />
       <div className="hidden md:flex border p-2 rounded-lg bg-gray-200 w-96">
         <input type="text" className="bg-transparent w-full" />
-        <Search />
+        <Search className="text-primary" />
       </div>
       {isSignedIn ? (
         <UserButton />
