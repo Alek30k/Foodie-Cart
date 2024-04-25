c3010f1943a624a9c869165e31bbfdea17032706
Alek30k <alemank23k@gmail.com>
2024-04-25 14:15:07 -0300

    last commit

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -1,1 +1,24 @@
+import { gpl, request } from "graphql-request";
+
 const MASTER_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
+const GetCategory = async () => {
+  const query = gpl`
+  query Categories {
+    categories(first: 50) {
+      id
+      name
+      slug
+      icon {
+        url
+      }
+    }
+  }`;
+
+  const result = await request(MASTER_URL, query);
+  return result;
+};
+
+export default {
+  GetCategory,
+};
