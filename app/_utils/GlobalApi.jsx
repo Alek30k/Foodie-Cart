51aab39dd476142d8a980ff77ce9b5a9079ab189
Alek30k <alemank23k@gmail.com>
2024-04-26 15:46:16 -0300

    last commit

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -19,7 +19,36 @@ const GetCategory = async () => {
   const result = await request(MASTER_URL, query);
   return result;
 };
+const GetBusiness = async (category) => {
+  const query =
+    gql`
+    query GetBusiness {
+      restaurants(where: { categories_some: { slug: "` +
+    category +
+    `" } }) {
+        aboutUs
+        address
+        banner {
+          url
+        }
+        categories {
+          name
+          id
+        }
+        id
+        name
+        restroType
+        slug
+        workingHours
+      }
+    }
+  `;
+
+  const result = await request(MASTER_URL, query);
+  return result;
+};
 
 export default {
   GetCategory,
+  GetBusiness,
 };
