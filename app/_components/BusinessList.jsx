18296411bd82a841b87779c360ed669626707e8f
Alek30k <alemank23k@gmail.com>
2024-04-27 14:23:04 -0300

    last commit

--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -2,16 +2,25 @@
 
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import GlobalApi from "../_utils/GlobalApi";
 
 const BusinessList = () => {
   const params = useSearchParams();
   const [category, setCategory] = useState("all");
+  const [businessList, setBusinessList] = useState();
 
   useEffect(() => {
     params && setCategory(params.get("category"));
+    params && getBusinessList(params.get("category"));
   }, [params]);
 
-  return <div className="">BusinessList</div>;
+  const getBusinessList = (category_) => {
+    GlobalApi.GetBusiness(category_).then((resp) => {
+      console.log(resp);
+    });
+
+    return <div className="">BusinessList</div>;
+  };
 };
 
 export default BusinessList;
