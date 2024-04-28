94e7226775b4404135d2d8c81caac41c747e030b
Alek30k <alemank23k@gmail.com>
2024-04-28 10:52:57 -0300

    last commit

---
+++ app/_components/BusinessItemSkeleton.jsx
@@ -0,0 +1,5 @@
+const BusinessItemSkeleton = () => {
+  return <div className="">BusinessItemSkeleton</div>;
+};
+
+export default BusinessItemSkeleton;
--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
 import BusinessItem from "./BusinessItem";
+import BusinessItemSkeleton from "./BusinessItemSkeleton";
 
 const BusinessList = () => {
   const params = useSearchParams();
@@ -28,9 +29,15 @@ const BusinessList = () => {
       <h2 className="font-bold text-2xl">Popular {category} Restaurants</h2>
       <h2 className="font-bold text-primary">{businessList?.length} Results</h2>
       <div className="grid grid-col sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 mt-3">
-        {businessList.map((restaurants, index) => (
-          <BusinessItem key={index} business={restaurants} />
-        ))}
+        {loading ? (
+          businessList.map((restaurants, index) => (
+            <BusinessItem key={index} business={restaurants} />
+          )):
+          
+          [1,2,3,4,5,6,7,8].map((item, index)=>(
+            <BusinessItemSkeleton/>
+          )
+       ) }
       </div>
     </div>
   );
