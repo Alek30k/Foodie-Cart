6c5f4802bc5c6bb8e765377d80be1fb1c91dbab5
Alek30k <alemank23k@gmail.com>
2024-04-28 15:34:15 -0300

    last commit

--- app/(routes)/restaurant/[name]/page.jsx
+++ app/(routes)/restaurant/[name]/page.jsx
@@ -1,5 +1,21 @@
+"use client";
+
+import GlobalApi from "@/app/_utils/GlobalApi";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
 const RestaurantDetails = () => {
-  const getRestaurantDetail = () => {};
+  const params = usePathname();
+
+  useEffect(() => {
+    console.log(params);
+  }, []);
+
+  const getRestaurantDetail = () => {
+    GlobalApi.GetBusinessDetail().then((resp) => {
+      console.log(resp);
+    });
+  };
 
   return <div className="">RestaurantDetails</div>;
 };
--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -34,7 +34,7 @@ const BusinessList = () => {
               <BusinessItem key={index} business={restaurants} />
             ))
           : [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
-              <BusinessItemSkeleton />
+              <BusinessItemSkeleton key={index} />
             ))}
       </div>
     </div>
