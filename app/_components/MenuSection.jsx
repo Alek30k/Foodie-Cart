108d110f356814f4038234b15ce9ca6833283b32
Alek30k <alemank23k@gmail.com>
2024-05-01 15:08:57 -0300

    last commit

--- app/_components/MenuSection.jsx
+++ app/_components/MenuSection.jsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const MenuSection = ({ restaurant }) => {
   return (
     <div className="">
       <div className="grid grid-cols-4 mt-2 ">
-        <div className="">Category</div>
+        <div className="hidden md:flex">
+          {restaurant?.menu?.map((item, index) => (
+            <Button>{item.category}</Button>
+          ))}
+        </div>
         <div className="col-span-3 ">Menu List</div>
       </div>
     </div>
