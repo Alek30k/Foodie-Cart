169018fd1079d118b1182a44f17167aea9f16c63
Alek30k <alemank23k@gmail.com>
2024-05-02 10:59:37 -0300

    last commit

--- app/_components/MenuSection.jsx
+++ app/_components/MenuSection.jsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useState } from "react";
 
 const MenuSection = ({ restaurant }) => {
+  const [menuItemList, setMenuItemList] = useState([]);
+
   const FilterMenu = (category) => {
-    console.log("object");
     const result = restaurant?.menu?.filter(
       (item) => item.category == category
     );
-    console.log(result);
+    setMenuItemList(result[0]);
   };
 
   return (
@@ -24,7 +29,29 @@ const MenuSection = ({ restaurant }) => {
             </Button>
           ))}
         </div>
-        <div className="col-span-3">Menu List</div>
+        <div className="col-span-3">
+          <h2 className="font-extrabold text-lg">{menuItemList.category}</h2>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            {menuItemList?.menuItem.map((item, undex) => (
+              <div className="p-2 flex gap-3 border rounded-lg">
+                <Image
+                  src={item?.productImage.url}
+                  alt={item.name}
+                  width={120}
+                  height={120}
+                  className="object-cover min-w-[120px] h-[120px] rounded-xl"
+                />
+                <div className="">
+                  <h2 className="font-bold">{item.name}</h2>
+                  <h2 className="">{item.price}</h2>
+                  <h2 className="text-sm text-gray-400 line-clamp-2">
+                    {item.description}
+                  </h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
