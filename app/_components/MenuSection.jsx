331e26ce39c02aa3cb3660098c46798a555da53f
Alek30k <alemank23k@gmail.com>
2024-05-02 10:31:18 -0300

    last commit

--- app/_components/MenuSection.jsx
+++ app/_components/MenuSection.jsx
@@ -1,15 +1,30 @@
 import { Button } from "@/components/ui/button";
 
 const MenuSection = ({ restaurant }) => {
+  const FilterMenu = (category) => {
+    console.log("object");
+    const result = restaurant?.menu?.filter(
+      (item) => item.category == category
+    );
+    console.log(result);
+  };
+
   return (
     <div className="">
       <div className="grid grid-cols-4 mt-2 ">
-        <div className="hidden md:flex">
+        <div className="hidden md:flex flex-col mr-10 gap-2">
           {restaurant?.menu?.map((item, index) => (
-            <Button>{item.category}</Button>
+            <Button
+              key={index}
+              variant="ghost"
+              className="flex justify-start"
+              onClick={() => FilterMenu(item.category)}
+            >
+              {item.category}
+            </Button>
           ))}
         </div>
-        <div className="col-span-3 ">Menu List</div>
+        <div className="col-span-3">Menu List</div>
       </div>
     </div>
   );
