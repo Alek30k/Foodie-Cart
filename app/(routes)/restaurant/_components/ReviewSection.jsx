9600982602d53808cca581c7421b127f743235e1
Alek30k <alemank23k@gmail.com>
2024-05-08 14:24:17 -0300

    last commit

--- app/(routes)/restaurant/_components/ReviewSection.jsx
+++ app/(routes)/restaurant/_components/ReviewSection.jsx
@@ -1,7 +1,12 @@
+import { Textarea } from "@/components/ui/textarea";
+
 const ReviewSection = ({ restaurant }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 mt-10">
-      <div className="">User Input</div>
+      <div className="flex flex-col gap-2">
+        <h2 className="font-bold text-lg">Add your review</h2>
+        <Textarea />
+      </div>
       <div className="col-span-2 ">List of Review</div>
     </div>
   );
