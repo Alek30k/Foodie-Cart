98df4805869af2fb5b6f6ef924e3e9d2d84fb011
Alek30k <alemank23k@gmail.com>
2024-05-10 11:29:08 -0300

    last commit

--- app/(routes)/checkout/page.jsx
+++ app/(routes)/checkout/page.jsx
@@ -13,6 +13,9 @@ const Checkout = () => {
   return (
     <div className="">
       <h2 className="font-bold text-2xl my-5">Checkaut</h2>
+      <div className="p-5 px-5 md:px-10 grid grid-cols-1 md:grid-cols-3 py-8">
+        <div className="md:col-span-2 mx-20"></div>
+      </div>
     </div>
   );
 };
