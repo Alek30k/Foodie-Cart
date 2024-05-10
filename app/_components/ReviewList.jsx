f57c6981c74ce4129f4577935436f62018205348
Alek30k <alemank23k@gmail.com>
2024-05-09 22:43:57 -0300

    last commit

--- app/_components/ReviewList.jsx
+++ app/_components/ReviewList.jsx
@@ -5,30 +5,38 @@ import moment from "moment";
 const ReviewList = ({ reviewList }) => {
   return (
     <div className="flex flex-col gap-5">
-      {reviewList &&
-        reviewList.map((review, index) => (
-          <div className="flex gap-5 items-center border" key={index}>
-            <Image
-              src={review.profileImage}
-              alt={review.profileImage}
-              width={50}
-              height={50}
-              className="rounded-full"
-            />
-            <div className="">
-              <h2 className="">{review?.reviewText}</h2>
-              <ReactRating
-                style={{ maxWidth: 100 }}
-                value={review.star}
-                isDisabled={true}
+      {reviewList
+        ? reviewList.map((review, index) => (
+            <div
+              className="flex gap-5 items-center border rounded-lg p-5"
+              key={index}
+            >
+              <Image
+                src={review.profileImage}
+                alt={review.profileImage}
+                width={50}
+                height={50}
+                className="rounded-full"
               />
-              <h2 className="text-sm">
-                <span className="font-bold">{review?.userName}</span> at
-                {moment(review.publishedAt).format("DD-MMM-yyyy")}
-              </h2>
+              <div className="">
+                <h2 className="">{review?.reviewText}</h2>
+                <ReactRating
+                  style={{ maxWidth: 100 }}
+                  value={review.star}
+                  isDisabled={true}
+                />
+                <h2 className="text-sm ">
+                  <span className="font-bold">{review?.userName}</span> at{" "}
+                  <span className=" text-gray-400">
+                    {moment(review.publishedAt).format("DD-MMM-yyyy")}
+                  </span>
+                </h2>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        : [1, 2, 3, 4].map((item, index) => (
+            <div className="h-[100px] w-full bg-slate-200 animate-pulse rounded-lg"></div>
+          ))}
     </div>
   );
 };
