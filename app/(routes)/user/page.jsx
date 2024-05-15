f33973c38ebc0a73273fed0d55f0777d74248894
Alek30k <alemank23k@gmail.com>
2024-05-15 16:57:07 -0300

    last commit

--- app/(routes)/user/page.jsx
+++ app/(routes)/user/page.jsx
@@ -1,9 +1,21 @@
-import { UserProfile } from "@clerk/nextjs";
+import { UserButton, UserProfile } from "@clerk/nextjs";
+import { DotIcon } from "lucide-react";
 
 const User = () => {
   return (
     <div className="flex justify-center items-center">
-      <UserProfile></UserProfile>
+      <UserProfile>
+        <UserButton.UserProfilePage
+          label="Terms"
+          labelIcon={<DotIcon />}
+          url="terms"
+        >
+          <div>
+            <h1>Custom Terms Page</h1>
+            <p>This is the custom terms page</p>
+          </div>
+        </UserButton.UserProfilePage>
+      </UserProfile>
     </div>
   );
 };
