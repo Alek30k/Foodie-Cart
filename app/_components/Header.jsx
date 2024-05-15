aaa75169a527324ed424bd7ea4f04f20c9d004b8
Alek30k <alemank23k@gmail.com>
2024-05-15 15:04:46 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -14,6 +14,14 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import Cart from "./Cart";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   const { user, isSignedIn } = useUser();
@@ -58,7 +66,26 @@ const Header = () => {
             </PopoverContent>
           </Popover>
 
-          <UserButton />
+          {/* <UserButton /> */}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Image
+                src={user?.imageUrl}
+                alt="user"
+                width={35}
+                height={35}
+                className="rounded-full"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       ) : (
         <div className="flex gap-5">
