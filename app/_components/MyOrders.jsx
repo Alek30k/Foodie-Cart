6f379c70e85500413edf9f6ac15f9cafd66d5cf5
Alek30k <alemank23k@gmail.com>
2024-05-16 10:41:40 -0300

    last commit

--- app/_components/MyOrders.jsx
+++ app/_components/MyOrders.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
 import { useUser } from "@clerk/nextjs";
+import moment from "moment";
 
 const MyOrders = () => {
   const { user } = useUser();
+  const [orderList, setOrderList] = useState([]);
 
   useEffect(() => {
     user && GetUserOrders();
@@ -12,12 +14,25 @@ const MyOrders = () => {
   const GetUserOrders = () => {
     GlobalApi.GetUserOrders(user?.primaryEmailAddress?.emailAddress).then(
       (resp) => {
-        console.log(resp?.orders);
+        setOrderList(resp?.orders);
       }
     );
   };
 
-  return <div className="">MyOrders</div>;
+  return (
+    <div className="">
+      <h2 className="font-bold text-lg">My Orders</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+        {orderList.map((order, index) => (
+          <div className="p-3 border rounded-lg" key={index}>
+            <h2 className="">
+              {moment(order?.createdAt).format("DD-MMM-yyyy")}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
