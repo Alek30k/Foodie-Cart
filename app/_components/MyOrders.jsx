82692cce14690281aac3eb7cf88e6e14364654f5
Alek30k <alemank23k@gmail.com>
2024-05-16 10:26:22 -0300

    last commit

--- app/_components/MyOrders.jsx
+++ app/_components/MyOrders.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import GlobalApi from "../_utils/GlobalApi";
+import { useUser } from "@clerk/nextjs";
+
 const MyOrders = () => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && GetUserOrders();
+  }, [user]);
+
+  const GetUserOrders = () => {
+    GlobalApi.GetUsersOrders().then((resp) => {
+      console.log(resp);
+    });
+  };
+
   return <div className="">MyOrders</div>;
 };
 
