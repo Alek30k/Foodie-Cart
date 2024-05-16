852b1e974800cc01f603c057cde2c7dfc3f9fc1f
Alek30k <alemank23k@gmail.com>
2024-05-16 11:08:48 -0300

    last commit

--- app/_components/MyOrders.jsx
+++ app/_components/MyOrders.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
 import { useUser } from "@clerk/nextjs";
 import moment from "moment";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const MyOrders = () => {
   const { user } = useUser();
@@ -14,6 +20,7 @@ const MyOrders = () => {
   const GetUserOrders = () => {
     GlobalApi.GetUserOrders(user?.primaryEmailAddress?.emailAddress).then(
       (resp) => {
+        console.log(resp);
         setOrderList(resp?.orders);
       }
     );
@@ -22,12 +29,45 @@ const MyOrders = () => {
   return (
     <div className="">
       <h2 className="font-bold text-lg">My Orders</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         {orderList.map((order, index) => (
-          <div className="p-3 border rounded-lg" key={index}>
-            <h2 className="">
+          <div
+            className="p-3 border rounded-lg flex flex-col gap-3"
+            key={index}
+          >
+            <h2 className="font-bold">
               {moment(order?.createdAt).format("DD-MMM-yyyy")}
             </h2>
+            <h2 className="flex justify-between text-sm">
+              Order Total Amount: <span>{(order?.orderAmount).toFixed(2)}</span>
+            </h2>
+            <h2 className="flex justify-between">
+              Address:{" "}
+              <span>
+                {order?.address}, {order?.zipCode}
+              </span>
+            </h2>
+
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  {" "}
+                  <h2 className="text-primary underline text-sm">
+                    View Order Detail
+                  </h2>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="">
+                    {order?.orderDetail?.map((item, index) => (
+                      <div className="" key={index}>
+                        <h2 className="">{item?.name}</h2>
+                        <h2 className="">{item?.price}</h2>
+                      </div>
+                    ))}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         ))}
       </div>
